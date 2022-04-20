95170713c1bef4d65ec1a2aabe61f210d0da35cd
jsepin <jerome.sepin@uzh.ch>
2022-04-20 11:51:27 +0200

    add original

---
+++ code_original/00_Function_LSTM.r
@@ -0,0 +1,66 @@
+# Loading required Packages
+require(keras)
+require(tensorflow)
+require(tidyverse)
+
+# Scaling: 
+scaling <- function(y){
+  # Y = Response Variable:
+  y_scaled <- (y-min(y))/(max(y)-min(y)) # Scaling Response funciton
+  return(y_scaled)
+}
+
+# Features and Label creator: 
+FeatLabCreator <- function(lag, y){
+  dataX = c()
+  dataY = c()
+
+  for(i in 1:(length(y)-lag)){
+    dataX <- c(dataX, y[i:(i+lag-1)])    # Feature
+    dataY <- c(dataY, y[i+lag])          # Label
+  }
+  dataX <- matrix(dataX, ncol = lag, byrow = T) # Feature as Matrix
+  dataX <- array(dataX, dim = c(nrow(dataX), lag, 1))
+  dataY <- array(dataY, dim = c(nrow(dataX), 1, 1))
+  return(list("Feature" = dataX,"Label" = dataY))
+}
+
+# Test for Training and validation:
+TrainValidTest <- function(Feature,Label, proportion){
+  index <- as.integer(nrow(Feature)*proportion)                   # Proportion
+  
+    # Splitting Feature into Train set, Validaton set:
+  Train_Feature <- Feature[1:index , ,]                           # Train set
+  Train_Feature <- array(Train_Feature, dim = c(nrow(Train_Feature), ncol(Train_Feature), 1))
+  
+  Validation_Feature <- Feature[(index+1):nrow(Feature) , ,]      # Validation set
+  Validation_Feature <- array(Validation_Feature, dim = c(nrow(Validation_Feature), ncol(Validation_Feature), 1))
+  
+    # Splitting Label into Train set, Validation set:
+  Train_Label <- Label[1:index , ,]                               # Train set
+  Train_Label <- array(Train_Label, dim = c(length(Train_Label), 1,1))
+  Validation_Label <- Label[(index+1):nrow(Feature) , ,]          # Validation set
+  Validation_Label <- array(Validation_Label, dim = c(length(Validation_Label), 1,1))
+  
+  return(list("Feature_Train" = Train_Feature, 
+              "Feature_Validation" = Validation_Feature,
+              "Label_Train" = Train_Label,
+              "Label_Validation" = Validation_Label))
+}
+
+
+# Prediction:
+pred_steps <- function(model, start, StepPred){
+  start_arr <- array(start, dim = c(1,length(start),1))
+  hist <- c()
+  for(i in 1:StepPred){
+    next_pred <- model %>% predict(start_arr)
+    hist <- c(hist,next_pred)
+    start <- c(start[2:length(start)],next_pred)
+    start_arr <- array(start,dim= c(1,length(start),1))
+  }
+  return(hist)
+}
+
+
+
---
+++ code_original/README.md
@@ -0,0 +1,14 @@
+# ESC403_DataScience
+Trying to commit (Jerome)
+
+
+1) What question will you try to answer with the project?
+We are trying to predict the number of deaths in Switzerland and evaluate our predictions with the actual observed deaths. Furhtermore we 
+
+2) What data will you be using (and what is the source of that data)?
+Covid19 data
+
+3) What techniques will you use for the analyses?
+
+*  EDA
+*  LSTM neural network
