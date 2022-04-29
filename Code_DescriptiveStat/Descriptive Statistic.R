60c5a62f9ea13ee74844661d469a2c183435de89
Vithersan Somasundaram <vithersan@MacBook-Pro-vsomas.local>
2022-04-29 17:14:46 +0200

    Descriptive Stats

--- Code_DescriptiveStat/Descriptive Statistic.R
+++ Code_DescriptiveStat/Descriptive Statistic.R
@@ -4,6 +4,7 @@
 # author: Vithersan Somasundaram
 #--------------------------------------------------------------------------------------------
 
+# Preparation ####
 # Clear the desk
 rm(list=ls())
 
@@ -13,37 +14,14 @@ dd <- read.csv("./data/pollution.csv")
 # Load Librarires:
 library(tidyverse)
 library(ggpubr)
-library(naniar)       # Overview of the data
-library(tableone)
-
+library(forecast)
 
 # tidy data
 dd$cbwd <- factor(dd$cbwd,
                   levels = c("cv", "NE", "NW", "SE"),
                   labels = c("CV","NE","NW","SE"))
 
-# Overview of the data:
-dd_miss <- dd
-colnames(dd_miss) <-c("No", "Year", "Month", "Day", "Hour", 
-                      "Particulate matter 2.5 µm", "Dew point", "Temperature", 
-                      "Air pressure", "cbwd", "Iws", "Is", "Ir")
-
-gg_miss_var(dd_miss, show_pct = TRUE)+
-  theme_linedraw()+
-  theme(axis.text.y = element_text(size = 10))
-
-
-# Characterisitcs of the variables
-variable = c("pm2.5", "DEWP", "TEMP", 
-"PRES", "cbwd", "Iws", "Is", "Ir")
-TableOne <- CreateTableOne(vars = variable, data = dd)
-TableOne <- print(TableOne, showAllLevels = TRUE)
-TableOne %>%
-  kbl()%>%
-  kable_classic(full_width = FALSE, html_font = "Cambria", position = "center")
-
-
-# Visualisation Response variable
+# Visualisation Response variable ####
 ggplot(data = dd, aes(x = seq(from = 1, to = length(dd$No), by= 1),y = TEMP))+
   geom_line(alpha = .5, colour = "black")+
   labs(x = "Observations", y = "Temperature")+
@@ -67,7 +45,40 @@ ggplot(data = dd, aes(x = seq(from = 1, to = length(dd$No), by= 1),y = TEMP))+
   annotate("segment", 
            x = 35059.2, xend = 35059.2,
            y = -20, yend = 50, colour = "darkblue")+
-
   # Label
   annotate("text", x = c(4382.4,13147.2,21912,30676.8,39441.6), y = 45, label = c("2010","2011","2012","2013","2014"))+
   theme_linedraw()
+
+
+
+# Introduction | Simulation visualising predicted future error  ####
+start <- dd$TEMP[2:50]
+for(i in 1:20){
+  start <- c(start, start[length(start)] + rnorm(n = 1, mean = 0, sd = .5))
+}
+
+par(mfrow=c(1,2))
+plot(dd$TEMP[2:100],type = "l", ylab = "Temperature [C°]", main = "" )
+lines(dd$TEMP[1:99]+ rnorm(n = 99, mean = 0, sd = .5),type = "l", col= 'darkred')
+legend("bottomleft", inset = .02, legend=c("Real Values", "Predicted Values"),
+       col=c("black", "darkred"), lty=c(1,1), cex=0.5, bty = "n")
+
+plot(dd$TEMP[2:100],type = "l", ylab = "Temperature [C°]", main = "" )
+lines(start, col = "darkblue")
+abline(v = 50, lty = 2)
+legend("bottomleft",inset = .02, legend=c("Real Values", "Predicted Values"),
+       col=c("black", "darkblue"), lty=c(1,1), cex=0.5, bty = "n")
+
+
+
+
+dd <- dplyr::select(dd, year,month, TEMP)
+dat <- array(data = c(unlist(dd)),
+             dim = c(43824,3,3),
+             dimnames = list(rownames(dd$year),
+                             colnames(dd$month)))
+ts.dat <- ts(dd$TEMP,start = c(2010), end=c(2014), frequency =24*365) #8760 / 24
+plot(ts.dat, ylab = "Temperature [C°]", main = "Meterological Records in Beijjing" )
+
+ts.dat <- ts(dd$TEMP,start = c(2010), end=c(2014), frequency =24)
+plot(ts.dat)
